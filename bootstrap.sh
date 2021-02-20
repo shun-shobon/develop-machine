e083cc38edba459cae638d9cde9669f05e1e8ea1
Shuntaro Nishizawa <me@shun.technology>
2021-02-21 03:57:54 +0900

    feat: add mirror list

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,16 +7,21 @@ timedatectl set-ntp true
 timedatectl set-timezone Asia/Tokyo
 
 # install necessary
-pacman -Syyu --noconfirm --needed base base-devel git zsh neovim tmux exa bat docker docker-compose
+pacman -Syyu --noconfirm --needed base base-devel reflector git zsh neovim tmux exa bat docker docker-compose
 
+# change mirror
+reflector --country 'Japan' --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+
+# enable docker
 systemctl enable docker
+usermod -aG docker vagrant
 
+# change locale
 cat <<EOF > /etc/locale.gen
 en_US.UTF-8 UTF-8
 ja_JP.UTF-8 UTF-8
 EOF
 locale-gen
 
+# change default shell
 chsh -s $(which zsh) vagrant
-
-usermod -aG docker vagrant
