efa51c831e788505476bd1576656daf393a351c9
Shuntaro Nishizawa <me@shun.technology>
2021-02-23 03:29:25 +0900

    fix: fix reflector

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ timedatectl set-timezone Asia/Tokyo
 pacman -Syyu --noconfirm --needed base base-devel reflector git zsh neovim tmux exa bat docker docker-compose
 
 # change mirror
-reflector --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --latest 20 --sort rate --protocol http --protocol https --save /etc/pacman.d/mirrorlist
 
 # enable docker
 systemctl enable docker
