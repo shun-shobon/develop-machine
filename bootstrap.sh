e7c7ec3dd4f1233633787800766fafb57ee5d5a2
Shuntaro Nishizawa <me@shun.technology>
2021-02-23 21:29:24 +0900

    feat: change docker to podman

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,14 +7,7 @@ timedatectl set-ntp true
 timedatectl set-timezone Asia/Tokyo
 
 # install necessary
-pacman -Syyu --noconfirm --needed base base-devel reflector git zsh neovim tmux exa bat docker docker-compose
-
-# change mirror
-reflector --latest 20 --sort rate --protocol http --protocol https --save /etc/pacman.d/mirrorlist
-
-# enable docker
-systemctl enable docker
-usermod -aG docker vagrant
+pacman -Syyu --noconfirm --needed base base-devel reflector git zsh tmux exa bat fuse-overlayfs podman podman-docker
 
 # change locale
 cat <<EOF > /etc/locale.gen
@@ -22,6 +15,18 @@ en_US.UTF-8 UTF-8
 ja_JP.UTF-8 UTF-8
 EOF
 locale-gen
+
+# change mirror
+reflector --latest 20 --sort rate --protocol http --protocol https --save /etc/pacman.d/mirrorlist
+
+# setup podman
+cat <<EOF > /etc/containers/registries.conf
+unqualified-search-registries = ["docker.io", "ghcr.io", "quay.io"]
+EOF
+sed -i -e '/^driver/c driver = "overlay"' -e "/^#mount_program/c mount_program = \"$(which fuse-overlayfs)\"" /etc/containers/storage.conf
+echo "vagrant:10000:65536" >> /etc/subuid
+echo "vagrant:10000:65536" >> /etc/subgid
+podman system migrate
 
 # change default shell
 chsh -s $(which zsh) vagrant
